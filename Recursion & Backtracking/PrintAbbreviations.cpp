85362f15729d57ed6fba5189c16c98c7d7b6d050
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-02-26 22:23:25 +0530

    Create PrintAbbreviations.cpp

---
+++ Recursion & Backtracking/PrintAbbreviations.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+    void solution(string str, string asf, int count, int pos)
+    {
+        if(str.length() == pos) {
+            if(count > 0)
+                asf += to_string(count);
+            cout<<asf<<endl;
+            return;
+        }
+
+        // yes
+        if(count == 0)
+            solution(str, asf + str[pos], count, pos + 1);
+        else
+            solution(str, asf + to_string(count) + str[pos], 0, pos + 1);
+
+        // no
+        solution(str, asf, count + 1, pos + 1);
+    }
+	int main() {
+        string str;
+        cin >> str;
+        solution(str,"",0,0);
+        return 0;
+    }

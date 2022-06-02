2e36be6c61b77ae3eef9396df7d2bd4f6de62e1d
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-06-02 22:22:50 +0530

    Create AllIndices.cpp

---
+++ Recursion & Backtracking/AllIndices.cpp
@@ -0,0 +1,15 @@
+
+int allIndexes(int input[], int size, int x, int output[]) {
+	
+    if(size == 0) 
+        return 0;
+    
+    int ans = allIndexes(input, size - 1, x, output);
+    
+    if(input[size - 1] == x) {
+        output[ans] = size - 1;
+        ans++;
+    }
+    
+    return ans;
+}

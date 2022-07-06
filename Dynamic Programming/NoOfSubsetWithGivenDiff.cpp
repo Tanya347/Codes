1e332b7af1120d58a0eb3263af90a8afabe22763
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-07-06 13:46:53 +0530

    Create NoOfSubsetWithGivenDiff.cpp

---
+++ Dynamic Programming/NoOfSubsetWithGivenDiff.cpp
@@ -0,0 +1,40 @@
+int subsetSum(vector<int> nums, int sum) {
+        
+    //normal code for subset sum
+        
+    int n = nums.size();
+    vector<vector<int>> subs(n + 1, vector<int>(sum + 1, 0));
+    
+    for(int i = 0; i <= n; i++) {
+        subs[i][0] = 1;
+    }
+        
+        
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= sum; j++) {
+            if(nums[i - 1] <= j) {
+                subs[i][j] = subs[i - 1][j] + subs[i - 1][j - nums[i - 1]];
+            } else {
+                subs[i][j] = subs[i - 1][j];
+            }
+        }
+    }
+            
+    return subs[n][sum];
+}
+
+int countSubset(vector<int> &nums, int diff) {
+    
+    //  calculate sum of entire array
+    int sum = 0;
+        
+    for(int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+    }
+    
+    int subsetsum = (diff + sum)/2;
+  
+    int count = subsetSum(nums, subsetsum);
+    
+    return count;
+}

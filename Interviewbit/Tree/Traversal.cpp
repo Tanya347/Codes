412dbe8e5eb0f6d73f0f77b62634010cb9489fb6
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-07-20 13:00:41 +0530

    Create Traversal.cpp

---
+++ Interviewbit/Tree/Traversal.cpp
@@ -0,0 +1,63 @@
+/********************************************************************** INORDER **************************************************************************************/
+
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+
+    vector<int> ans;
+
+    if(A == NULL)
+        return ans;
+
+    vector<int> l = inorderTraversal(A -> left);
+
+    ans = l;
+
+    ans.push_back(A -> val);
+
+    vector<int> r = inorderTraversal(A -> right);
+
+    ans.insert(ans.end(), r.begin(), r.end());
+
+    return ans;
+}
+
+/********************************************************************** PREORDER **************************************************************************************/
+
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+    vector<int> ans;
+
+    if(A == NULL)
+        return ans;
+
+    ans.push_back(A -> val);
+    
+    vector<int> l = preorderTraversal(A -> left);
+    vector<int> r = preorderTraversal(A -> right);
+    
+    ans.insert(ans.end(), l.begin(), l.end());
+    ans.insert(ans.end(), r.begin(), r.end());
+
+    return ans;
+
+}
+
+/********************************************************************** POST ORDER **************************************************************************************/
+
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    vector<int> ans;
+
+    if(A == NULL)
+        return ans;
+        
+    vector<int> l = postorderTraversal(A -> left);
+    
+    ans = l;
+    
+    vector<int> r = postorderTraversal(A -> right);
+
+    ans.insert(ans.end(), r.begin(), r.end());
+
+    ans.push_back(A -> val);
+
+    return ans;
+
+}

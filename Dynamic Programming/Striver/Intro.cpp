a82916956c5fe80915d6cfa12a4d6b22253e4be0
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-10 19:22:14 +0530

    Update Intro.cpp

--- Dynamic Programming/Striver/Intro.cpp
+++ Dynamic Programming/Striver/Intro.cpp
@@ -1,3 +1,13 @@
+/********************************************************************** FIBONACCI **************************************************************************************/
+
+/*
+
+
+
+*/
+
+/********************************************************************** ANSWER **************************************************************************************/
+
 class Solution {
   public:
   int m = 1e9+7;
@@ -26,3 +36,102 @@ class Solution {
         return dp[n]%m;
     }
 };
+
+/********************************************************************** QUESTION **************************************************************************************/
+
+/*
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+
+    1 <= n <= 45
+
+*/
+
+/********************************************************************** BRUTE FORCE **************************************************************************************/
+
+// 31/45 test cases passed
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2)
+            return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
+
+/********************************************************************** MEMOIZATION **************************************************************************************/
+
+// 34.56 speed 29.54 memory
+
+class Solution {
+public:
+    
+    int climbStairs_mem(int n, int* dp) {
+        if(n <= 2) {
+            dp[n] = n;
+            return dp[n];
+        }
+        if(dp[n] != -1)
+            return dp[n];
+        
+        dp[n] = climbStairs_mem(n - 1, dp) + climbStairs_mem(n - 2, dp);
+        return dp[n];
+    }
+    
+    int climbStairs(int n) {
+        int *dp = new int[n + 1];
+        for(int i = 0; i < n + 1; i++) {
+            dp[i] = -1;
+        }
+        return climbStairs_mem(n, dp);
+    }
+
+
+};
+
+/********************************************************************** DP **************************************************************************************/
+
+// Speed 34.56% Memory 15.2%
+
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+
+        if(n <= 2)
+            return n;
+        
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < n + 1; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+

d8ed72ca79f6cd2877d93481b66c935c9362f6a9
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-17 19:25:28 +0530

    Update 01Matrix.cpp

--- Graphs/Striver/01Matrix.cpp
+++ Graphs/Striver/01Matrix.cpp
@@ -33,6 +33,9 @@ Constraints:
 
 /********************************************************************** DP **************************************************************************************/
 
+// TC: O(2*M*N) ~ O(M*N) 
+// SC: O(M*N)
+
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
@@ -42,6 +45,7 @@ public:
         
         vector<vector<int>> distance(m, vector<int>(n, INT_MAX - 1));
         
+        // first travel in the top left to bottom right direction and find the min distance of upper and left zeroes
         for(int r = 0; r < m; r++) {
             for(int c = 0; c < n; c++) {
                 if(mat[r][c] == 0) {
@@ -56,6 +60,7 @@ public:
             }
         }
         
+        // then we move from bottom right to top left direction and find min distance of zeroes this time considering bottom and right 0s
         for(int r = m - 1; r >= 0; r--) {
             for(int c = n - 1; c >= 0; c--) {
                 if(r != m - 1)
@@ -65,11 +70,19 @@ public:
             }
         }
         
+        // we can simply return distance
         return distance;
     }
 };
 
+// it may come to your mind why not compute for all 4 directions at once, well the data in first traversal on top and left keeps getting updated but the one on 
+// right and bottom doesn't so we can't use that data as of now, so in the second traversal top left data is updated and bottom right data keeps getting updated so we 
+// have all the data we need then
+
 /********************************************************************** BFS **************************************************************************************/
+
+// TC: O(M*N + M*N*4) ~ O(M*N) 
+// SC: O(M*N)
 
 class Solution {
 public:

505a5ece86120d99c3b1a03e1b207389e5acd949
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-18 11:24:25 +0530

    Update ChocolatesPickup.cpp

--- Dynamic Programming/Striver/ChocolatesPickup.cpp
+++ Dynamic Programming/Striver/ChocolatesPickup.cpp
@@ -186,3 +186,55 @@ class Solution {
         return rec(0, 0, n - 1, grid, dp);
     }
 };
+    
+/********************************************************************** SPACE OPT **************************************************************************************/
+    
+class Solution {
+  public:
+  
+  
+    int solve(int m, int n, vector<vector<int>>& grid) {
+        // code here
+        
+        vector<vector<int>> prev(n, vector<int>(n, 0));
+        
+        for(int j1 = 0; j1 < n; j1++) {
+            for(int j2 = 0; j2 < n; j2++) {
+                if(j1 == j2)
+                prev[j1][j2] = grid[m - 1][j2];
+                else
+                prev[j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
+            }
+        }
+        
+        
+        for(int i = m - 2; i >= 0; i--) {
+            
+            vector<vector<int>> curr(n, vector<int>(n, 0));
+            for(int j1 = 0; j1 < n; j1++) {
+                for(int j2 = 0; j2 < n; j2++) {
+                    int maxi = -1e8;
+                    
+                    for(int dj1 = -1; dj1 < 2; dj1++) {
+                        for(int dj2 = -1; dj2 < 2; dj2++) {
+                            
+                            int temp = -1e8;
+                            
+                            if(j1 + dj1 >= 0 && j2 + dj2 >= 0 && (j1 + dj1) < n && (j2 + dj2) < n) {
+                                if(j1 == j2)
+                                    temp = grid[i][j1] + prev[j1 + dj1][j2 + dj2];
+                                else
+                                    temp = grid[i][j1] + grid[i][j2] + prev[j1 + dj1][j2 + dj2];
+                            }
+                            maxi = max(temp, maxi);
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            prev = curr;
+        }
+        
+        return prev[0][n - 1];
+    }
+};

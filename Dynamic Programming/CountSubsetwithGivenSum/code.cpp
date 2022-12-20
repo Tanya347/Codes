399ec75b48b059897e92e44b16a50990b5770d48
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-20 19:30:51 +0530

    Update code.cpp

--- Dynamic Programming/CountSubsetwithGivenSum/code.cpp
+++ Dynamic Programming/CountSubsetwithGivenSum/code.cpp
@@ -1,3 +1,33 @@
+// gfg
+
+int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        
+        vector<vector<long long int>> dp(n + 1, vector<long long int>(sum + 1, 0));
+        int mod = (int)1e9 + 7;
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            
+            // we can't ignore 0 as it can also be the sum i.e. an element can be 0
+            // when we are considering sum to be 0 then there are two ways possible, one is 
+            // including the 0 element and other is not including it i.e keeping an empty subset
+            for(int j = 0; j <= sum; j++) {
+                if(arr[i - 1] <= j)
+                    dp[i][j] = (dp[i - 1][j - arr[i - 1]] + dp[i - 1][j])%mod;
+                else
+                    dp[i][j] = (dp[i - 1][j])%mod;
+            }
+        }
+        
+        return dp[n][sum]%mod;
+	}
+
+/************************************************************************************************************************************************************/
+
 int isSubSet(int arr[],int n,int sum){
     
     int dp[n+1][sum+1];
@@ -26,3 +56,4 @@ int isSubSet(int arr[],int n,int sum){
     return dp[n][sum];
 
 }
+

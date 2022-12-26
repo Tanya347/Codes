2c86a788a8e4853ed18310cf7de6f9b5f3106105
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-26 21:49:45 +0530

    Update LongestCommonSubstring.cpp

--- Dynamic Programming/LongestCommonSubstring.cpp
+++ Dynamic Programming/LongestCommonSubstring.cpp
@@ -1,3 +1,39 @@
+
+/******************************************************************* QUESTION *******************************************************************************/
+
+/*
+
+Given two strings. The task is to find the length of the longest common substring.
+
+
+Example 1:
+
+Input: S1 = "ABCDGH", S2 = "ACDGHR", n = 6, m = 6
+Output: 4
+Explanation: The longest common substring
+is "CDGH" which has length 4.
+Example 2:
+
+Input: S1 = "ABC", S2 "ACB", n = 3, m = 3
+Output: 1
+Explanation: The longest common substrings
+are "A", "B", "C" all having length 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function longestCommonSubstr() which takes the string S1, string S2 and their length n and m as inputs and returns the length of the longest common substring in S1 and S2.
+
+
+Expected Time Complexity: O(n*m).
+Expected Auxiliary Space: O(n*m).
+
+
+Constraints:
+1<=n, m<=1000
+
+*/
+
+/******************************************************************* ANSWER *******************************************************************************/
+
 int longestCommonSubstr (string S1, string S2, int n, int m)
     {
         // your code here
@@ -22,3 +58,35 @@ int longestCommonSubstr (string S1, string S2, int n, int m)
         
         return max_length;
 }
+
+/******************************************************************* SPACE OPT *******************************************************************************/
+
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        // your code here
+        vector<int> prev(m + 1, 0);
+        int length = INT_MIN;
+        
+        for(int i = 1; i <= n; i++) {
+            
+            vector<int> curr(m + 1, 0);
+            
+            for(int j = 1; j <= m; j++) {
+                
+                if(S1[i - 1] == S2[j - 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                    length = max(length, curr[j]);
+                }
+                else
+                    curr[j] = 0;
+            }
+            
+            prev = curr;
+        }
+        
+        return length;
+    }
+};

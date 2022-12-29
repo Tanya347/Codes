0b2abb3715b148beee9d4c423313be0c8aa32d6c
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-29 10:04:09 +0530

    Create PQ.cpp

---
+++ Graphs/Striver/Djikstra's Algorithm/PQ.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        vector<int> dist(V, 1e9);
+        
+        dist[S] = 0;
+        pq.push({0, S});
+        
+        while(!pq.empty()) {
+            int d = pq.top().first;
+            int n = pq.top().second;
+            pq.pop();
+            
+            for(auto it: adj[n]) {
+                
+                int adjNode = it[0];
+                int adjWeight = it[1];
+                
+                if(dist[adjNode] > d + adjWeight) {
+                    
+                    dist[adjNode] = d + adjWeight;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+        
+        return dist;
+    }
+};

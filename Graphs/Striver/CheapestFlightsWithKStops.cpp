d32f9a5e0408d909a804187f41447dc86c7f8c99
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2022-12-29 15:46:33 +0530

    Update CheapestFlightsWithKStops.cpp

--- Graphs/Striver/CheapestFlightsWithKStops.cpp
+++ Graphs/Striver/CheapestFlightsWithKStops.cpp
@@ -49,7 +49,7 @@ src != dst
 
 */
 
-/******************************************************************* ANSWER *******************************************************************************/
+/******************************************************************* ANS 1 *******************************************************************************/
 
 class Solution {
 public:
@@ -93,3 +93,44 @@ public:
 };
 
 /******************************************************************* ANS 2 *******************************************************************************/
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<pair<int, int>> adj[n];
+        for(auto it: flights) {
+            adj[it[0]].push_back({it[1], it[2]});
+        }   
+
+        queue<pair<int, pair<int, int>>> q;
+
+        vector<int> price(n, 1e9);
+
+        q.push({0, {0, src}});
+        price[src] = 0;
+
+        while(!q.empty()) {
+            
+            int stop = q.front().first;
+            int p = q.front().second.first;
+            int node = q.front().second.second;
+            
+            q.pop();
+
+            if(stop > k) continue;
+
+            for(auto it: adj[node]) {
+                int adjNode = it.first; 
+                int adjWeight = it.second; 
+                    
+                if(adjWeight + p < price[adjNode] && stop <= k) {
+                    price[adjNode] = adjWeight + p;
+                    q.push({stop + 1, {price[adjNode], adjNode}});
+                }
+            } 
+        }
+
+        return price[dst] == 1e9? -1 : price[dst];
+    }
+};

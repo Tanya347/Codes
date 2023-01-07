302e5bd288e894da8f21b798fb0926ebd7f027cd
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-07 17:55:55 +0530

    Update BuyAndSellStock3.cpp

--- Dynamic Programming/Striver/BuyAndSellStock3.cpp
+++ Dynamic Programming/Striver/BuyAndSellStock3.cpp
@@ -87,23 +87,6 @@ public:
 class Solution {
 public:
 
-    int maxProfit(vector<int>& prices, int buy, int cap, int i, vector<vector<vector<int>>>& dp) {
-
-        int n = prices.size();
-        
-        if(i == n || cap == 0)
-            return 0;
-        
-        if(dp[i][buy][cap] != -1)
-            return dp[i][buy][cap];
-        
-        if(buy == 0)
-            return dp[i][buy][cap] = max(maxProfit(prices, 0, cap, i + 1, dp), -prices[i] + maxProfit(prices, 1, cap, i + 1, dp));
-        else
-            return dp[i][buy][cap] = max(maxProfit(prices, 1, cap, i + 1, dp), prices[i] + maxProfit(prices, 0, cap - 1, i + 1, dp));
-        
-    }
-
     int maxProfit(vector<int>& prices) {
 
         vector<vector<vector<int>>> dp(prices.size() + 1, vector<vector<int>>(2, vector<int>(3, 0)));

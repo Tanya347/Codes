ca887b66c661574f8019e109e51bdd6bb90e6aee
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-07 10:55:15 +0530

    Update BuyAndSellStock2.cpp

--- Dynamic Programming/Striver/BuyAndSellStock2.cpp
+++ Dynamic Programming/Striver/BuyAndSellStock2.cpp
@@ -90,4 +90,105 @@ public:
     }
 };
 
-/******************************************************************* QUESTION *******************************************************************************/
+/******************************************************************* RECURSION *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices, int i, int buy) {
+        if(i == prices.size())
+            return 0;
+        
+        int profit = 0;
+        if(buy == 0) {
+            profit = max(maxProfit(prices, i + 1, 0), -prices[i] + maxProfit(prices, i + 1, 1));
+        } 
+        if(buy == 1) {
+            profit = max(maxProfit(prices, i + 1, 1), prices[i] + maxProfit(prices, i + 1, 0));
+        }
+
+        return profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        return maxProfit(prices, 0, 0);
+    }
+};
+
+/******************************************************************* MEMOISATION *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices, int i, int buy, vector<vector<int>>& dp) {
+        
+        if(i == prices.size())
+            return 0;
+        
+        if(dp[i][buy] != -1)
+            return dp[i][buy];
+
+        if(buy == 0) {
+            dp[i][buy] = max(maxProfit(prices, i + 1, 0, dp), -prices[i] + maxProfit(prices, i + 1, 1, dp));
+        } 
+        if(buy == 1) {
+            dp[i][buy] = max(maxProfit(prices, i + 1, 1, dp), prices[i] + maxProfit(prices, i + 1, 0, dp));
+        }
+
+        return dp[i][buy];
+    }
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return maxProfit(prices, 0, 0, dp);
+    }
+};
+
+/******************************************************************* DP *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, 0));
+
+        for(int i = prices.size() - 1; i >= 0; i--) {
+            for(int j = 0; j <= 1; j++) {
+                if(j == 0)
+                    dp[i][j] = max(dp[i + 1][0], -prices[i] + dp[i + 1][1]);
+                else
+                    dp[i][j] = max(dp[i + 1][1], prices[i] + dp[i + 1][0]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+/******************************************************************* SPACE OPT *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<int> ahead(2, 0);
+
+        for(int i = prices.size() - 1; i >= 0; i--) {
+
+            vector<int> curr(2, 0);
+            for(int j = 0; j <= 1; j++) {
+                if(j == 0)
+                    curr[j] = max(ahead[0], -prices[i] + ahead[1]);
+                else
+                    curr[j] = max(ahead[1], prices[i] + ahead[0]);
+            }
+
+            ahead = curr;
+        }
+
+        return ahead[0];
+    }
+};

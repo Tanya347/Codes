8af6df9a8ea6c48a5d6dda86cca521f1a164de8f
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-07 17:49:05 +0530

    Update BuyAndSellStock3.cpp

--- Dynamic Programming/Striver/BuyAndSellStock3.cpp
+++ Dynamic Programming/Striver/BuyAndSellStock3.cpp
@@ -30,9 +30,127 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 */
 
-/******************************************************************* ANSWER *******************************************************************************/
+/******************************************************************* BRUTE FORCE *******************************************************************************/
 
+class Solution {
+public:
 
+    int maxProfit(vector<int>& prices, int buy, int cap, int i) {
+
+        int n = prices.size();
+        if(i == n || cap == 0)
+            return 0;
+        
+        if(buy == 0)
+            return max(maxProfit(prices, 0, cap, i + 1), -prices[i] + maxProfit(prices, 1, cap, i + 1));
+        else
+            return max(maxProfit(prices, 1, cap, i + 1), prices[i] + maxProfit(prices, 0, cap - 1, i + 1));
+        
+    }
+
+    int maxProfit(vector<int>& prices) {
+        return maxProfit(prices, 0, 2, 0);
+    }
+};
+
+/******************************************************************* MEMOIZATION *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices, int buy, int cap, int i, vector<vector<vector<int>>>& dp) {
+
+        int n = prices.size();
+        
+        if(i == n || cap == 0)
+            return 0;
+        
+        if(dp[i][buy][cap] != -1)
+            return dp[i][buy][cap];
+        
+        if(buy == 0)
+            return dp[i][buy][cap] = max(maxProfit(prices, 0, cap, i + 1, dp), -prices[i] + maxProfit(prices, 1, cap, i + 1, dp));
+        else
+            return dp[i][buy][cap] = max(maxProfit(prices, 1, cap, i + 1, dp), prices[i] + maxProfit(prices, 0, cap - 1, i + 1, dp));
+        
+    }
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
+        return maxProfit(prices, 0, 2, 0, dp);
+    }
+};
+
+/******************************************************************* DP *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices, int buy, int cap, int i, vector<vector<vector<int>>>& dp) {
+
+        int n = prices.size();
+        
+        if(i == n || cap == 0)
+            return 0;
+        
+        if(dp[i][buy][cap] != -1)
+            return dp[i][buy][cap];
+        
+        if(buy == 0)
+            return dp[i][buy][cap] = max(maxProfit(prices, 0, cap, i + 1, dp), -prices[i] + maxProfit(prices, 1, cap, i + 1, dp));
+        else
+            return dp[i][buy][cap] = max(maxProfit(prices, 1, cap, i + 1, dp), prices[i] + maxProfit(prices, 0, cap - 1, i + 1, dp));
+        
+    }
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<vector<int>>> dp(prices.size() + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        
+        for(int i =  prices.size() - 1; i >= 0; i--) {
+            for(int j = 0; j <= 1; j++) {
+                for(int k = 1; k <= 2; k++) {
+                    if(j == 0)
+                        dp[i][j][k] = max(dp[i + 1][j][k], -prices[i] + dp[i + 1][1][k]);
+                    else
+                        dp[i][j][k] = max(dp[i + 1][j][k], prices[i] + dp[i + 1][0][k - 1]);
+                }
+            }
+        }
+        
+        return dp[0][0][2];
+    }
+};
+
+/******************************************************************* SPACE OPT *******************************************************************************/
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        
+        for(int i =  prices.size() - 1; i >= 0; i--) {
+
+            vector<vector<int>> curr(2, vector<int>(3, 0));
+
+            for(int j = 0; j <= 1; j++) {
+                for(int k = 1; k <= 2; k++) {
+                    if(j == 0)
+                        curr[j][k] = max(ahead[j][k], -prices[i] + ahead[1][k]);
+                    else
+                        curr[j][k] = max(ahead[j][k], prices[i] + ahead[0][k - 1]);
+                }
+            }
+
+            ahead = curr;
+        }
+        
+        return ahead[0][2];
+    }
+};
 
 /******************************************************************* MERA DIMAG (193/214) *******************************************************************************/
 // where it went wrong

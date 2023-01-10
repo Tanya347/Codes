6caee3f9be5990694eb2f64f2efda1f69efc4f28
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-10 20:28:44 +0530

    Update LIS.cpp

--- Dynamic Programming/Striver/LIS.cpp
+++ Dynamic Programming/Striver/LIS.cpp
@@ -176,3 +176,31 @@ public:
         return ans;
     }
 };
+
+/******************************************************************* BINARY SEARCH *******************************************************************************/
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int arr[])
+    {
+       // your code here
+       vector<int> temp;
+       temp.push_back(arr[0]);
+       int len = 1;
+       
+       for(int i = 1; i < n; i++) {
+           if(arr[i] > temp.back()) {
+              temp.push_back(arr[i]);
+              len++;
+           }
+           else {
+              int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+              temp[ind] = arr[i];
+           }
+       }
+       
+       return len;
+    }
+};

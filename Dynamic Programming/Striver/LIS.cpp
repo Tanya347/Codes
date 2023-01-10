6c015cb8f9e3a700cf51313688ccc90a33135846
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-10 20:04:31 +0530

    Update LIS.cpp

--- Dynamic Programming/Striver/LIS.cpp
+++ Dynamic Programming/Striver/LIS.cpp
@@ -85,6 +85,73 @@ public:
     }
 };
 
+/******************************************************************* DP *******************************************************************************/
+
+class Solution {
+public:
+ 
+    int lengthOfLIS(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1,vector<int>(n + 1,0));
+    
+        for(int ind = n - 1; ind >= 0; ind--){
+            for (int prev_index = ind - 1; prev_index >= -1; prev_index--){
+                
+                int notTake = 0 + dp[ind + 1][prev_index + 1];
+        
+                int take = 0;
+        
+                if(prev_index == -1 || arr[ind] > arr[prev_index]){
+                    
+                    take = 1 + dp[ind + 1][ind + 1];
+                }
+        
+                dp[ind][prev_index + 1] = max(notTake,take);
+                
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
+
+/******************************************************************* SPACE OPT *******************************************************************************/
+
+class Solution {
+public:
+
+    int lengthOfLIS(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ahead(n + 1,0);
+    
+        for(int ind = n - 1; ind >= 0; ind--){
+
+            vector<int> curr(n + 1, 0);            
+
+            for (int prev_index = ind - 1; prev_index >= -1; prev_index--){
+                
+                int notTake = 0 + ahead[prev_index + 1];
+        
+                int take = 0;
+        
+                if(prev_index == -1 || arr[ind] > arr[prev_index]){
+                    
+                    take = 1 + ahead[ind + 1];
+                }
+        
+                curr[prev_index + 1] = max(notTake,take);
+                
+            }
+
+            ahead = curr;
+        }
+        
+        return ahead[0];
+    }
+};
+
 /******************************************************************* LOOP *******************************************************************************/
 
 class Solution {

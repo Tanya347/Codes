391e550e92085758d1dff7733ce465a82932da8e
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-01-14 18:50:37 +0530

    Update Traversal.cpp

--- Binary Trees/Striver/Traversal.cpp
+++ Binary Trees/Striver/Traversal.cpp
@@ -56,3 +56,47 @@ vector <int> postOrder(Node* root)
 }
 
 /******************************************************************* LEVEL ORDER *******************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        if(root == NULL)
+            return {};
+            
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        vector<vector<int>> ans;
+        
+        while(!nodes.empty()) {
+            int s = nodes.size();
+            vector<int> temp;
+            while(s--) {
+                TreeNode* node = nodes.front();
+                nodes.pop();
+                temp.push_back(node -> val);
+
+                if(node -> left) 
+                    nodes.push(node -> left);
+                
+                if(node -> right)
+                    nodes.push(node -> right);
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};

ef53f302652071061e19e1ad822b0645bd211bb3
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-08-01 23:21:11 +0530

    Update ChocolatesPickup.cpp

--- Dynamic Programming/Striver/ChocolatesPickup.cpp
+++ Dynamic Programming/Striver/ChocolatesPickup.cpp
@@ -140,53 +140,58 @@ class Solution {
   
  /********************************************************************** DP **************************************************************************************/
 
-  class Solution {
-  public:
-  
-  
-    int rec(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+  int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
         
-        if(j1 < 0 || j2 < 0 || j1 >= n || j2 >= n)
-            return -1e8;
-            
         // base case
-        if(i == m - 1) {
-            if(j1 == j2)
-                return grid[i][j1];
-            else 
-                return grid[i][j1] + grid[i][j2];
-        }
-        
-        if(dp[i][j1][j2] != -1)
-        return dp[i][j1][j2];
-        
-        int maxi = 0;
-        for(int dj1 = -1; dj1 < 2; dj1++) {
-            for(int dj2 = -1; dj2 < 2; dj2++) {
-                int temp;
-                
+        for(int j1 = 0; j1 < n; j1++) {
+            for(int j2 = 0; j2 < n; j2++) {
                 if(j1 == j2)
-                    temp = grid[i][j1] + rec(i + 1, j1 + dj1, j2 + dj2, grid, dp);
-                else
-                    temp = grid[i][j1] + grid[i][j2] + rec(i + 1, j1 + dj1, j2 + dj2, grid, dp);
-                
-                maxi = max(maxi, temp);
+                    dp[m - 1][j1][j2] = grid[m - 1][j1];
+                else 
+                    dp[m - 1][j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
             }
         }
-        
-        return dp[i][j1][j2] = maxi;
-    }
-  
-    int solve(int m, int n, vector<vector<int>>& grid) {
-        // code here
-        
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        return rec(0, 0, n - 1, grid, dp);
+
+        // tabulation
+
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j1 = 0; j1 < n; j1++) {
+                for(int j2 = 0; j2 < n; j2++) {
+
+                    int maxi = -1e8;
+
+                    for(int dj1 = -1; dj1 <= 1; dj1++) {
+                        for(int dj2 = -1; dj2 <= 1; dj2++) {
+                            
+                            int temp;
+                            if (j1 == j2)
+                                temp = grid[i][j1];
+                            else
+                                temp = grid[i][j1] + grid[i][j2];
+
+                            if ((j1 + dj1 < 0 || j1 + dj1 >= n) ||
+                                (j2 + dj2 < 0 || j2 + dj2 >= n))
+
+                                temp += -1e9;
+                            else
+                                temp += dp[i + 1][j1 + dj1][j2 + dj2];
+
+                            maxi = max(temp, maxi);
+                        }
+                    }
+
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][n - 1];
     }
 };
-    
+
 /********************************************************************** SPACE OPT **************************************************************************************/
     
 class Solution {

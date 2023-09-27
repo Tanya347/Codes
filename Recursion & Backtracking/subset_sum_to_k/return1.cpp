d7e9dcbff581b8fe024b6f21fb12f5731bfcd28c
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-09-27 15:23:30 +0530

    Update return1.cpp

--- Recursion & Backtracking/subset_sum_to_k/return1.cpp
+++ Recursion & Backtracking/subset_sum_to_k/return1.cpp
@@ -55,3 +55,54 @@ int subsetSumToK(int input[], int n, int output[][50], int k) {
     //without first element
     return s1+s2;
 }
+
+/******************************************************************* EXPLANATION ************************************************************************/
+
+Recursion Idea:
+The main idea behind this approach is the principle of inclusion-exclusion. For each number in the array, you have two choices:
+
+Include the number in the subset.
+Exclude the number from the subset.
+You recursively process the array and check both of these possibilities.
+
+Dry Run:
+
+Consider the sample input:
+
+css
+Copy code
+n = 9 
+input[] = 5 12 3 17 1 18 15 3 17 
+K = 6
+Let's dry run the function for a few steps to understand its behavior.
+
+When the function is called for the first time:
+input[] = 5 12 3 17 1 18 15 3 17
+n = 9
+k = 6
+
+First, we'll explore the subsets that include the first element, 5.
+So, the new call becomes: input + 1, n - 1, k - input[0]
+i.e., for input 12 3 17 1 18 15 3 17 and k = 1 (because 6 - 5 = 1)
+
+On the second level, now we'll see the subsets that include the first element, 12.
+But 12 is greater than 1, so no valid subset can be formed. We only explore the excluding part now.
+
+So, the next call will be: input + 1, n - 1, k for input 3 17 1 18 15 3 17 and k = 1.
+
+Continuing this process, the third level will include 3 and look for subsets summing to -2, which is invalid. So, we'll explore the excluding part for input 17 1 18 15 3 17 and k = 1.
+
+At the fourth level, we'll skip 17 (since it's greater than the target) and look at the subset with the next element: 1 18 15 3 17 and k = 1.
+
+At this point, we reach the number 1 which is equal to our target. So, one of the valid subsets we've found is [5, 1].
+
+This process continues until the end of the array.
+
+Output Formulation:
+For each valid subset, the code maintains an array where the first index tells you how many elements are there in that subset (let's call it size) and the next indexes (from 1 to size) contain the actual elements of the subset.
+
+The recursive calls essentially bifurcate the output array: one half where the first element is included (output) and the other half where it's excluded (output + s1). After each recursion, the code modifies the size (the first index) of each subset and inserts the current element at the beginning of the subset if it's included.
+
+After recursively processing the entire array, the function will have populated the output array with all subsets that sum to K.
+
+In our dry run, the function will find [5, 1] and [3, 3] as the subsets of the input array that sum to 6.

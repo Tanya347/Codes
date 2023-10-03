6a691e8df961b12ec214730e130be3ab3b8082cd
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-10-03 15:08:27 +0530

    Update ShortestSubsequence.cpp

--- Dynamic Programming/ShortestSubsequence.cpp
+++ Dynamic Programming/ShortestSubsequence.cpp
@@ -19,3 +19,40 @@ Explanation:
 "aab" is the shortest subsequence which is present in S and absent in V.
 
 /************************************************* ANSWER *****************************************************************************/
+
+#include<vector>
+
+int solve(string s, string v) {
+    // Write your code here
+    int n = s.size();
+    int m = v.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    vector<vector<int>> next(n + 1, vector<int>(m + 1));
+
+    for(int i = 0; i < n; i++) {
+        int prev = -1;
+        for(int j = 0; j < m; j++) {
+            if(s[i] == v[j])
+                prev = j;
+            next[i + 1][j + 1] = prev;
+        }
+    }
+    
+    for(int i = 1; i <= n; i++)
+        dp[i][0] = 1;
+    
+    for(int i = 0; i <= m; i++)
+        dp[0][i] = 1e9;
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(next[i][j] == -1)
+                dp[i][j] = 1;
+            else
+                dp[i][j] = min(dp[i - 1][j], 1 + dp[i - 1][next[i][j]]);
+        }
+    }
+
+    return dp[n][m];
+}

a4e6d9d5b457ec794c57b38333c586b473784d72
Tanya Dixit <80675137+Tanya347@users.noreply.github.com>
2023-10-03 14:12:47 +0530

    Update MaximumSquareMatrixWithAllZeroes.cpp

--- Dynamic Programming/MaximumSquareMatrixWithAllZeroes.cpp
+++ Dynamic Programming/MaximumSquareMatrixWithAllZeroes.cpp
@@ -32,3 +32,40 @@ Sample Output 2:
 
 /************************************************* ANSWER *****************************************************************************/
 
+ int findMaxSquareWithAllZeros(int **arr, int n, int m)
+{
+	//Write your code here
+	int **dp = new int*[row];
+	for(int i = 0; i < row; i++) 
+		dp[i] = new int[col];
+
+	// check first col if array is 1 make dp 0 and vice versa
+	for(int i = 0; i < row; i++)
+		dp[i][0] = arr[i][0] == 1 ? 0 : 1;
+	
+	// check first row if array is 1 make dp 0 and vice versa
+	for(int j = 1; j < col; j++)
+		dp[0][j] = arr[0][j] == 1 ? 0 : 1;
+
+	// 1 is used to maintain incremental sizes
+	for(int i = 1; i < row; i++) {
+		for(int j = 1; j < col; j++) {
+			if(arr[i][j] == 0)
+			// if this element is a part of a valid square 
+				dp[i][j] = min(dp[i - 1][j], min[dp[i - 1][j - 1], dp[i][j - 1]]) + 1;
+			else
+				dp[i][j] = 0;
+		}
+	}
+
+	int max = 0;
+	for(int i = 0; i < row; i++) {
+		for(int j = 0; j < col; j++) {
+			if(dp[i][j] > max)
+				max = dp[i][j];
+		}
+	}
+
+	return max;
+}
+
